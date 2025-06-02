c87eef78951954c015ca48d5ec32c622a2bd5c6c
Minmin20112 <tuanminh20112@gmail.com>
2025-06-02 23:56:20 +0700

    Assignment4 Part 1: Completed

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
@@ -13,7 +14,27 @@ void* threadfunc(void* thread_param)
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+     struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+    // Sleep before trying to lock
+    usleep(thread_func_args->wait_to_obtain_ms * 1000);
+
+    if (pthread_mutex_lock(thread_func_args->mutex) != 0) {
+        ERROR_LOG("Failed to lock mutex\n");
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+
+    // Sleep while holding the lock
+    usleep(thread_func_args->wait_to_release_ms * 1000);
+
+    if (pthread_mutex_unlock(thread_func_args->mutex) != 0) {
+        ERROR_LOG("Failed to unlock mutex\n");
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+
+    thread_func_args->thread_complete_success = true;
     return thread_param;
 }
 
@@ -28,6 +49,22 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    bool success = true;
+    struct thread_data* data = (struct thread_data*)malloc(sizeof(struct thread_data));
+    memset(data,0,sizeof(struct thread_data));
+    data->wait_to_obtain_ms=wait_to_obtain_ms;
+    data->wait_to_release_ms = wait_to_release_ms;
+    data->mutex=mutex;
+    
+    
+    int rc = pthread_create(thread, NULL, // Use default attributes
+                                    threadfunc,
+                                    data);
+    if ( rc != 0 ) {
+        ERROR_LOG("pthread_create failed with error %d\n", rc);
+        free(data);
+        success = false;
+    	}
+    return success; 
 }
 
